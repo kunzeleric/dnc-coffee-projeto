6273fdedc213abc11b3b530ff833a33675c5a2d2
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-01-26 22:21:59 -0300

    Projecto DNC Coffee V2

--- script.js
+++ script.js
@@ -2,56 +2,53 @@
 
 //let botao = null;
 let cards = Array.from(document.querySelectorAll(".card"));
-let buttons = Array.from(document.querySelectorAll('.comprar'));
+let buttons = Array.from(document.querySelectorAll(".comprar"));
+let cardText = Array.from(document.querySelectorAll(".card-text"));
 
 cards.forEach((element, index) => {
-    
   element.addEventListener("mouseenter", () => {
-//    if (botao !== null) {
-//      botao.remove();
-//    }
+
+    //Estiliza os elementos para que a altura dos botões seja igual em todos cards.
+    cardText[index].style.height= "110px";
     buttons[index].style.visibility = "visible";
     buttons[index].style.height = "auto";
-
-    //botao = document.createElement("div");
-    //botao.innerHTML = `<button class="comprar">COMPRAR AGORA</button>`;
-    //element.append(botao);
   });
 
   element.addEventListener("mouseleave", () => {
-    //botao.remove();
+    //Seta as configurações para o modelo "original".
+    cardText[index].style.height= "auto";
     buttons[index].style.visibility = "hidden";
     buttons[index].style.height = "0";
- });
-
+  });
 });
 
 //Modal do botão Atendimento
 let modalCard = null;
-let modal = document.querySelector('#modal')
-let modalPlace = document.querySelector('#modalPlace');
+let modal = document.querySelector("#modal");
+let modalPlace = document.querySelector("#modalPlace");
 
+//Checa se o modal já existe, para não acumular um modal em cima de outro.
 modal.addEventListener("click", () => {
+  if (modalCard !== null) {
+    modalCard.remove();
+  }
 
-    if(modalCard !== null) {
-        modalCard.remove();
-    }
+  //Cria uma div para inserir o conteúdo do modal dentro.
+  modalCard = document.createElement("div");
+  modalPlace.style.visibility = "visible";
 
-    modalCard = document.createElement("div");
-    modalPlace.style.visibility = 'visible';
-
-    modalCard.innerHTML = `<div class="modalAtendimento">
+  modalCard.innerHTML = `<div class="modalAtendimento">
     <button><img src="assets/images/close-btn.png" onClick="closeModal()"></button>
     <h1>Atendimento</h1>
     <p>Rua José de Alencar, 111, Centro<br>
     CEP: 12209-000 | +55 (11) 3333-8000</p>
      <button>ENTRAR EM CONTATO</button>
-    <div>`
+    <div>`;
 
-    modalPlace.append(modalCard)
-
+  //Adiciona o modalCard dentro do elemento declarado no HTML
+  modalPlace.append(modalCard);
 });
 
 closeModal = () => {
-  modalPlace.style.visibility = 'hidden';
-}
+  modalPlace.style.visibility = "hidden";
+};
